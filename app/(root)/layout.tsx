b87d7a6fc3fdcd605b3ac5e3e703a82885eed157
EpicIbby101 <kevin_grittner@outlook.com>
2024-01-10 17:48:22 +0000

    Finished setting up dashboard

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,11 @@
 import prismadb from "@/lib/prismadb";
-import { auth, redirectToSignIn } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-export default async function DashboardLayout({
+export default async function SetupLayout({
   children,
-  params,
 }: {
   children: React.ReactNode;
-  params: { storeId: string };
 }) {
   const { userId } = auth();
 
@@ -17,14 +15,13 @@ export default async function DashboardLayout({
 
   const store = await prismadb.store.findFirst({
     where: {
-      id: params.storeId,
       userId,
     },
   });
 
-  if (!store) {
-    redirect("/");
+  if (store) {
+    redirect(`/${store.id}`);
   }
 
-  return <div>{children}</div>;
+  return <>{children}</>;
 }
