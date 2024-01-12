acfcf35c494ebd9f9451ae53b95cce49efe0ea0b
EpicIbby101 <kevin_grittner@outlook.com>
2024-01-12 20:39:27 +0000

    Populated navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,26 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
 import { MainNav } from "./main-nav";
+import StoreSwitcher from "./store-switcher";
+import { redirect } from "next/navigation";
+import prismadb from "@/lib/prismadb";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
   return (
     <div className="border-b">
       <div className="flex h-14 items-center px-4">
-        <div>Store Switcher</div>
-        <MainNav />
+        <StoreSwitcher items={stores} />
+        <MainNav className="mx-5" />
         <div className="ml-auto items-center space-x-4 pr-3">
           <UserButton afterSignOutUrl="/" />
         </div>
---
+++ components/store-switcher.tsx
@@ -0,0 +1,123 @@
+"use client";
+
+import { Store } from "@prisma/client";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useParams, useRouter } from "next/navigation";
+import {
+  Check,
+  ChevronsUpDown,
+  Command,
+  PlusCircle,
+  StethoscopeIcon,
+  Store as StoreIcon,
+} from "lucide-react";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
+import {
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "./ui/command";
+
+type PopoverTriggerProps = React.ComponentPropsWithoutRef<
+  typeof PopoverTrigger
+>;
+// Declare an interface named StoreSwitcherProps which extends PopoverTriggerProps which starts with an empty object.
+interface StoreSwitcherProps extends PopoverTriggerProps {
+  items: Store[];
+}
+// We'll need to also declare the PopoverTriggerProps type which is equals to React.ComponentPropsWithoutRef<typeof PopoverTrigger>(We'll need to import PopoverTrigger now)
+// Next we need to populate the empty object with items: store[]. The store is imported from prisma.
+// We want to add props to the StoreSwitcher, className and items as an empty array.
+// After the props we want to add StoreSwitcherProps
+
+export default function StoreSwitcher({
+  className,
+  items = [],
+}: StoreSwitcherProps) {
+  const storeModal = useStoreModal();
+  const params = useParams();
+  const router = useRouter();
+
+  const formattedItems = items.map((item) => ({
+    label: item.name,
+    value: item.id,
+  }));
+
+  const currentStore = formattedItems.find(
+    (item) => item.value === params.storeId
+  );
+
+  const [open, setOpen] = useState(false);
+
+  const onStoreSelect = (store: { value: string; label: string }) => {
+    setOpen(false);
+    router.push(`/${store.value}`);
+  };
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          size="sm"
+          role="combobox"
+          aria-expanded={open}
+          aria-label="Select a store"
+          className={cn("w-[200px] justify-between", className)}
+        >
+          <StoreIcon className="mr-2 h-4 w-4" />
+          {currentStore?.label}
+          <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandList>
+            <CommandInput placeholder="Search Store..." />
+            <CommandEmpty>No Store Found</CommandEmpty>
+            <CommandGroup>
+              {formattedItems.map((store) => (
+                <CommandItem
+                  key={store.value}
+                  onSelect={() => onStoreSelect(store)}
+                  className="text-sm"
+                >
+                  <StoreIcon className="mr-2 h-4 w-4" />
+                  {store.label}
+                  <Check
+                    className={cn(
+                      "ml-auto h-4 w-4",
+                      currentStore?.value === store.value
+                        ? "opacity-100"
+                        : "opacity-0"
+                    )}
+                  ></Check>
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+          <CommandSeparator />
+          <CommandList>
+            <CommandGroup>
+              <CommandItem
+                onSelect={() => {
+                  setOpen(false);
+                  storeModal.onOpen();
+                }}
+              >
+                <PlusCircle className="mr-2 h-5 w-5" />
+                Create Store
+              </CommandItem>
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
