70741b7cc98e18ad10b3359a61f5003566a86beb
EpicIbby101 <kevin_grittner@outlook.com>
2024-01-25 23:22:24 +0000

    Added cloudinary library

---
+++ components/image-upload.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { useState, useEffect } from "react";
+
+interface ImageUploadProps {
+  disabled?: boolean;
+  onChange: (value: string) => void;
+  onRemove: (value: string) => void;
+  value: string[];
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({
+  disabled,
+  onChange,
+  onRemove,
+  value,
+}) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <div>
+      <div>ok</div>
+    </div>
+  );
+};
+
+export default ImageUpload;
