e0753407919e59c7385b694f94fd061844f309ee
EpicIbby101 <kevin_grittner@outlook.com>
2024-01-30 19:34:06 +0000

    Added data tables

--- components/billboards-client.tsx
+++ components/billboards-client.tsx
@@ -5,21 +5,34 @@ import { Button } from "./ui/button";
 import { Heading } from "./ui/heading";
 import { Separator } from "./ui/separator";
 import { useParams, useRouter } from "next/navigation";
+import { Billboard } from "@prisma/client";
+import { BillboardColumn, columns } from "./columns";
+import { DataTable } from "./ui/data-table";
 
-const BillboardsClient = () => {
+interface BillboardClientProps {
+  data: BillboardColumn[];
+}
+
+const BillboardsClient: React.FC<BillboardClientProps> = ({ data }) => {
   const router = useRouter();
   const params = useParams();
 
   return (
     <>
       <div className="flex items-center justify-between">
-        <Heading title="Billboards" description="Manage Your Billboards" />
-        <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
+        <Heading
+          title={`Billboards (${data.length})`}
+          description="Manage Your Billboards"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
           <Plus className="mr-2" />
           Add New
         </Button>
       </div>
       <Separator />
+      <DataTable columns={columns} data={data}/>
     </>
   );
 };
